6384b40be765feafd1efb13e5fabf586c9cc138d
Dieter Deysel <ddeysel@gmail.com>
2025-09-23 08:29:10 +0200

    refactor

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ def main():
     filepath = 'books/frankenstein.txt'
     book_text = get_book_text(filepath)
     num_words = get_book_words(book_text)
-    num_chars = get_book_characters(book_text)
+    char_count_dict = get_book_characters(book_text)
+    print_report(filepath, num_words, char_count_dict)
+
+def print_report(filepath, num_words, char_count_dict):
     print(f"{num_words} words found in the document")  
-    print(get_book_characters(book_text)) 
+    print(char_count_dict) 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for entry in sort_characters_by_count(num_chars):
+    for entry in sort_characters_by_count(char_count_dict):
         if not entry['char'].isalpha():  # Only print alphabetic characters
             continue
         print(f"{entry['char']}: {entry['num']}")
